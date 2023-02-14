b908d077c276485e46a6f8c3eb6ab9bcbabf918f
BLACKHENZ29 <ayehenz29@gmail.com>
2023-02-14 14:30:15 +0000

    Added A Page For Viewing every single EVTOL with data table

---
+++ src/Page/allEvtol.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import "../Stylesheet/allev.css";
+import axios from "axios";
+import SideBar from "../Component/dashSide";
+import DataTable from "react-data-table-component";
+
+const EvtolList = () => {
+  const [evtols, setEvtols] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(
+        "http://localhost:4000/api/v1/evtol/admin/all"
+      );
+      setEvtols(result.data.data);
+    };
+    fetchData();
+  }, []);
+  const columns = [
+    {
+      name: "Serial Number",
+      selector: "serialNo",
+    },
+    {
+      name: "Model",
+      selector: "model",
+    },
+    {
+      name: "Weight",
+      selector: "weight",
+      cell: (row) => `${row.weight}kg`,
+    },
+    {
+      name: "Battery",
+      selector: "battery",
+      cell: (row) => `${row.battery}%`,
+    },
+    {
+      name: "State",
+      selector: "state",
+    },
+  ];
+  return (
+    <>
+      <div className="wrpp">
+        <SideBar />
+        <div className="main">
+          <p>All EVTOL DEVICES</p>
+          <div className="evwrap">
+            <DataTable columns={columns} data={evtols} pagination />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EvtolList;
+
