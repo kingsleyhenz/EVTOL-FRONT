5b1f0227d0b93a619a31ad3d4643e18cc8669b7d
BLACKHENZ29 <ayehenz29@gmail.com>
2024-01-18 22:28:31 +0100

    Login endpoint

--- src/Page/User/login.js
+++ src/Page/User/login.js
@@ -1,10 +1,41 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import "../../Stylesheet/User/login.css";
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { FcGoogle } from "react-icons/fc";
 import { GiDeliveryDrone } from "react-icons/gi";
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const Login =()=>{
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+    const baseUrl = 'https://droneservice.onrender.com'
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(`${baseUrl}/api/v1/user/login`, {
+                email,
+                password
+            });
+            if(response.status === 200){
+                toast.success("Login Successful👋");
+                localStorage.setItem("auth_token", response.data.token);
+                localStorage.setItem("role", response.data.role)
+                navigate('/make-a-request');
+            } else {
+                toast.error(response.data.message);
+            }
+        } catch (error) {
+            if (error.response && error.response.data) {
+                toast.error(error.response.data.message);
+            } else {
+                toast.error('Something went wrong. Please try again.');
+            }
+        }
+    };
     return(
         <>
             <div className='log-body'>
@@ -22,16 +53,16 @@ const Login =()=>{
                                 <p>or</p>
                                 <div id='line'></div>
                             </div>
-                            <form className='log-form'>
+                            <form className='log-form' onSubmit={handleSubmit}>
                                 <div className='log-holder'>
                                     <label>Email</label>
-                                    <input type="text" />
+                                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
                                 </div>
                                 <div className='log-holder'>
                                     <label>Password</label>
-                                    <input type="text" />
+                                    <input type="text" value={password} onChange={(e) => setPassword(e.target.value)}/>
                                 </div>
-                                <button>Login</button>
+                                <button type='submit'>Login</button>
                             </form>
                             <p>Haven't Signed Up Yet? <span> <NavLink to="/register-account">Sign Up</NavLink> </span></p>
                         </div>
