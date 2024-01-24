cb5368a5489753f66f71dfe4d082f44834e624b7
BLACKHENZ29 <ayehenz29@gmail.com>
2024-01-24 09:53:50 +0100

    User Request Form Restyle

--- src/Component/requestModal.js
+++ src/Component/requestModal.js
@@ -5,20 +5,16 @@ import "../Stylesheet/User/user-add.css";
 function RequestMod() {
   const iconStyle = { width: rem(12), height: rem(12) };
 
-  // Array of tab values
   const tabValues = ["first", "second", "third", "fourth", "fifth"];
 
-  // State to track the current tab index
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  // Function to handle Next button click
   const handleNext = () => {
     if (currentIndex < tabValues.length - 1) {
       setCurrentIndex((prevIndex) => prevIndex + 1);
     }
   };
 
-  // Function to handle Back button click
   const handleBack = () => {
     if (currentIndex > 0) {
       setCurrentIndex((prevIndex) => prevIndex - 1);
@@ -30,7 +26,7 @@ function RequestMod() {
       color="orangered"
       variant="pills"
       radius="sm"
-      value={tabValues[currentIndex]} // Set the current tab value based on the index
+      value={tabValues[currentIndex]}
       orientation="vertical"
       className="root"
       placement="right"
@@ -45,34 +41,86 @@ function RequestMod() {
       </Tabs.List>
 
       <Tabs.Panel value="first" className="panel">
-        <input type="text" placeholder="Parcel Country" />
-        <input type="text" placeholder="Parcel State" />
-        <input type="text" placeholder="Parcel City" />
-        <input type="text" placeholder="Parcel Address" />
+        <div className="panel-inp">
+          <label>Parcel Country</label>
+          <input type="text" placeholder="Parcel Country" />
+        </div>
+        <div className="panel-inp">
+          <label>Parcel State</label>
+          <input type="text" placeholder="Parcel State" />
+        </div>
+        <div className="panel-inp">
+          <label>Parcel City</label>
+          <input type="text" placeholder="Parcel City" />
+        </div>
+        <div className="panel-inp">
+          <label>Parcel Address</label>
+          <input type="text" placeholder="Parcel Address" />
+        </div>
       </Tabs.Panel>
 
       <Tabs.Panel value="second" className="panel">
-        <input type="text" placeholder="Recipient Country" />
-        <input type="text" placeholder="Recipient State" />
-        <input type="text" placeholder="Recipient City" />
-        <input type="text" placeholder="Recipient Address" />
+        <div className="panel-inp">
+          <label>Recipient Country</label>
+          <input type="text" placeholder="Recipient Country" />
+        </div>
+        <div className="panel-inp">
+          <label>Recipient State</label>
+          <input type="text" placeholder="Recipient State" />
+        </div>
+        <div className="panel-inp">
+          <label>Recipient City</label>
+          <input type="text" placeholder="Recipient City" />
+        </div>
+        <div className="panel-inp">
+          <label>Recipient Address</label>
+          <input type="text" placeholder="Recipient Address" />
+        </div>
       </Tabs.Panel>
 
       <Tabs.Panel value="third" className="panel shorter">
-        <input type="text" placeholder="Recipient Name" />
-        <input type="text" placeholder="Recipient Email" />
-        <input type="text" placeholder="Recipient Phone" />
+        <div className="panel-inp">
+          <label>Recipient Name</label>
+          <input type="text" placeholder="Recipient Name" />
+        </div>
+        <div className="panel-inp">
+          <label>Recipient Email</label>
+          <input type="text" placeholder="Recipient Email" />
+        </div>
+        <div className="panel-inp">
+          <label>Recipient Phone</label>
+          <input type="text" placeholder="Recipient Phone" />
+        </div>
       </Tabs.Panel>
 
       <Tabs.Panel value="fourth" className="panel">
-        <input type="text" placeholder="Parcel Name" />
-        <input type="number" placeholder="Parcel Width" />
-        <input type="number" placeholder="Parcel Length" />
-        <input type="number" placeholder="Parcel Height" />
+        <div className="panel-inp">
+          <label>Parcel Name</label>
+          <input type="text" placeholder="Parcel Name" />
+        </div>
+        <div className="panel-inp">
+          <label>Parcel Width</label>
+          <input type="number" placeholder="Parcel Width" />
+        </div>
+        <div className="panel-inp">
+          <label>Parcel Length</label>
+          <input type="number" placeholder="Parcel Length" />
+        </div>
+        <div className="panel-inp">
+          <label>Parcel Height</label>
+          <input type="number" placeholder="Parcel Height" />
+        </div>
       </Tabs.Panel>
 
       <Tabs.Panel value="fifth" className="panel">
-        <Textarea resize="vertical" placeholder="Parcel Description" className="p-desc" />
+        <div className="panel-inp">
+          <label>Parcel Description</label>
+          <Textarea
+            resize="vertical"
+            placeholder="Parcel Description"
+            className="p-desc"
+          />
+        </div>
       </Tabs.Panel>
     </Tabs>
   );
